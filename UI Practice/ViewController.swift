3be9e45e7d505b60051f76ebd505d234990a8ef3
Alphonso Bret Sensley II <fonzman1984@me.com>
2017-11-29 14:29:20 -0800

    Update: Updated UI placed pickerView instead of textField. Updated Code to use pickerView value to build URL

--- UI Practice/ViewController.swift
+++ UI Practice/ViewController.swift
@@ -12,29 +12,55 @@ import Foundation
 
 
 
-class ViewController: UIViewController {
-    var CrimeReport: Dictionary<String, String> = [:]
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    @IBOutlet weak var citySelection: UITextField!
-    var tableArray = [String] ()
-   
+    //LIST OF CITIES TO CHOOSE FROM
+    var AlamedaCountyCities = ["OAKLAND","FREMONT","HAYWARD","BERKELEY","SAN LEANDRO","LIVERMORE","PLEASANTON","ALAMEDA","UNION CITY","DUBLIN","NEWARK","ALBANY","EMERYVILLE","PIEDMONT"]
 
+   //PICKERVIEW TO CHOOSE CITIES
+    @IBOutlet weak var citySelection: UIPickerView?
+    var city: String = ""
+    
+    //MARK: PICKERVIEW METHODS
+    
+        //NUMBER OF SECTIONS
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+        //NUMBER OF ROWS
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return AlamedaCountyCities.count
+    }
+        //TITLE OF SAID ROWS
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return AlamedaCountyCities[row]
+    }
+        //ASSIGN ROW VALUES TO CITY VARIABLE TO BE USED IN URL
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        city = AlamedaCountyCities[row]
+        }
+
+    
+//VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.citySelection?.dataSource = self
+        self.citySelection?.delegate = self
         
     }
     
-    
+    // GET DATA ACTION
     @IBAction func GetCrimeDataButton(_ sender: Any) {
         parseJSON()
     }
     
-    typealias JSON = [String : String]
-    
+    //RETRIEVE DATA METHOD
     func parseJSON() {
-        var userCity = citySelection.text!
-        let urlString = "https://data.acgov.org/resource/js8f-yfqf.json?city=\(userCity)"
+        
+        let userCity = "?city=\(city)"
+        
+        let urlString = "https://data.acgov.org/resource/js8f-yfqf.json\(userCity)"
         let url = URL(string: urlString)
         
         
@@ -46,13 +72,15 @@ class ViewController: UIViewController {
             guard let unwrappedData = data else {return}
             do {
                 let responseJSON = try JSONSerialization.jsonObject(with: unwrappedData, options: .allowFragments)
-                
+               
                 print("Got Data")
                 print(responseJSON)
+                
                 
             } catch {
                 print(error)
             }
+            
         }
         task.resume()
         }
