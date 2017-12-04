d1a067793edc3f586cd3191180818f55346e3c09
Alphonso Bret Sensley II <fonzman1984@me.com>
2017-12-04 13:55:48 -0800

    UPDATE: Updated code in APIData.
    seperated method for parseJSON by created a constructURL method to handle building the URL.
    
    The parseJSON method handles the creation of the session

--- UI Practice/APIData.swift
+++ UI Practice/APIData.swift
@@ -20,15 +20,17 @@ struct CrimeReportData {
 }
 
 // ARRAY OF TYPE CRIMEREPORTDATA
-var CrimeReports = [CrimeReportData].self
+var CrimeReports = [String:AnyObject].self
+var urlString = ""
 
 class APIData: MainViewController {
     
     //API TOKEN
     private let token = "?$$app_token=BEyC2oP4D1T8CaKegklvf4ExN"
-     //var city: String = ""
-    //RETRIEVE DATA METHOD
-    func parseJSON() {
+    
+    //METHOD FOR BUILDING API URL
+    func buildUrl(constructedUrl: String) -> URL{
+        let baseURL = "https://data.acgov.org/resource/js8f-yfqf.json"
         //URL FILTER LIMITS RESULTS RETURNED
         let numberOfResults = "&$limit=10"
         //URL FILTER ORDERS BY MOST RECENT
@@ -37,22 +39,29 @@ class APIData: MainViewController {
         let userCity = "&city=\(city)"
        // print(MainViewController.CityString().city)
         
-        let urlString = "https://data.acgov.org/resource/js8f-yfqf.json\(token)\(userCity)\(sortOrder)\(numberOfResults)"
+        urlString = "\(baseURL)\(token)\(userCity)\(sortOrder)\(numberOfResults)"
         let FormattedUrlString = urlString.replacingOccurrences(of: " ", with: "%20")
         let url = URL(string: FormattedUrlString)
-      
+        return url!
         
-        let unwrappedURL = url
+    }
+    
+    
+    func parseJSON() {
+        let unwrappedURL = buildUrl(constructedUrl: urlString)
         //URL SESSION
         let session = URLSession.shared
-        let task = session.dataTask(with: (unwrappedURL)!) { (data, response, error) in
+        let task = session.dataTask(with: (unwrappedURL)) { (data, response, error) in
             print("Start")
             guard let unwrappedData = data else {return}
             do {
+                //JSONDATA IS AN ARRAY OF DICTIONARIES
                 let jsonData = try JSONSerialization.jsonObject(with: unwrappedData, options: []) as? [[String: AnyObject]]
+                //CRIMESTATS SHEDS THE ARRAY AND BECOMES A DICTIONARY
+                let crimeStats = jsonData![0]
                 
                 print("Got Data")
-                print(jsonData)
+                print(crimeStats)
                 } catch {
                     
                     
