4008782d2b2a772f81b69d3067a0f30ca6bd355e
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-02-10 00:08:18 -0800

    update: Updating UI to meet AppStore Specs. Corrected iPad viewing by add native iPad support 1,2

--- View/SlideOverMenu.swift
+++ View/SlideOverMenu.swift
@@ -26,7 +26,7 @@ class SlideOverMenu: UIViewController {
     override func viewDidLoad() {
         latitude = [manager.location!.coordinate.latitude]
         longitude = [manager.location!.coordinate.longitude]
-        let DataAlert = UIAlertController(title: "ATTENTION", message: "Crime data retrieved outside of Alameda County, CA may be out of date or unavailable", preferredStyle: UIAlertControllerStyle.actionSheet)
+        let DataAlert = UIAlertController(title: "ATTENTION", message: "Crime data retrieved outside of Alameda County, CA may be out of date or unavailable", preferredStyle: UIAlertControllerStyle.alert)
         DataAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.cancel, handler: nil))
         APIData().parseJSONLocation()
         present(DataAlert, animated: true, completion: nil)
